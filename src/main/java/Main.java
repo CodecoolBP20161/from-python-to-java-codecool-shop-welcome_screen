552b9dcd0bb288c21c55221b52f7160147925e88
vogronicspatrik <vogronicspatrik@gmail.com>
2016-11-08 10:45:45 +0100

    example done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,15 +33,25 @@ public class Main {
         supplierDataStore.add(amazon);
         Supplier lenovo = new Supplier("Lenovo", "Computers");
         supplierDataStore.add(lenovo);
+        Supplier apple = new Supplier("Apple", "Digital content and services");
+        supplierDataStore.add(apple);
+
 
         //setting up a new product category
         ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
         productCategoryDataStore.add(tablet);
+        ProductCategory phones = new ProductCategory("Phones", "Hardware", "Smartphone.");
+        productCategoryDataStore.add(phones);
+        ProductCategory watches = new ProductCategory("Watches", "Hardware", "Smartwatch.");
+        productCategoryDataStore.add(watches);
+
 
         //setting up products and printing it
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("Iwatch", 389, "USD", "", tablet, apple));
+        productDataStore.add(new Product("HTC", 115, "USD", "best phone ever", tablet, amazon));
 
     }
 
