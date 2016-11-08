9bb302fca3823f7c2835dc138eef82efb968b920
kalmeee <revkalman@gmail.com>
2016-11-08 13:53:54 +0100

    basic adding logic

--- src/main/java/com/codecool/shop/model/ShoppingCart.java
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -1,7 +1,22 @@
 package com.codecool.shop.model;
 
+import java.util.HashMap;
+
 /**
  * Created by kalman on 2016.11.08..
  */
 public class ShoppingCart {
+    HashMap<Product, Integer> lineItems;
+
+    public ShoppingCart() {
+    }
+
+    public  void add(Product product) {
+        Integer value = lineItems.get(product);
+        if (value != null) {
+            lineItems.put(product, lineItems.get(product) + 1);
+        } else {
+            lineItems.put(product, 1);
+        }
+    }
 }
