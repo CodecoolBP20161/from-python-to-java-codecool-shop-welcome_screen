418acf2dd36d8a1a1ca730436e1ddb23f58fb265
kalmeee <revkalman@gmail.com>
2016-11-22 10:36:27 +0100

    class structure added

---
+++ src/main/java/com/codecool/shop/dao/Implementation/JdbcImpl/SupplierDaoJdbc.java
@@ -0,0 +1,31 @@
+package com.codecool.shop.dao.Implementation.JdbcImpl;
+
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Supplier;
+
+import java.util.List;
+
+/**
+ * Created by kalman on 2016.11.22..
+ */
+public class SupplierDaoJdbc implements SupplierDao{
+    @Override
+    public void add(Supplier category) {
+
+    }
+
+    @Override
+    public Supplier find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Supplier> getAll() {
+        return null;
+    }
+}
