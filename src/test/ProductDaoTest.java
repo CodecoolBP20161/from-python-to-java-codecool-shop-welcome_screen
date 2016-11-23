888705cd3528fd09fbe6f9fc7cce3d42455267e5
seradam <seradam89@gmail.com>
2016-11-23 11:36:52 +0100

    test skeleton and dependencies

---
+++ src/test/ProductDaoTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by seradam on 2016.11.23..
+ */
+public class ProductDaoTest {
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void find() throws Exception {
+
+    }
+
+    @Test
+    public void remove() throws Exception {
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+
+    }
+
+    @Test
+    public void getBy() throws Exception {
+
+    }
+
+    @Test
+    public void getBy1() throws Exception {
+
+    }
+
+}
