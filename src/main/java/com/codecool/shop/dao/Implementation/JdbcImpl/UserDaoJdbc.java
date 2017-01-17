d3770f0df2de2e93c0fbf74b6e9c4e6b50233b74
kalmeee <revkalman@gmail.com>
2017-01-17 10:58:40 +0100

    user basic model and jdbc interface with impl skeleton

---
+++ src/main/java/com/codecool/shop/dao/Implementation/JdbcImpl/UserDaoJdbc.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.dao.Implementation.JdbcImpl;
+
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.model.Supplier;
+import com.codecool.shop.model.User;
+
+import java.util.HashMap;
+
+/**
+ * Created by kalman on 2017.01.17..
+ */
+public class UserDaoJdbc implements UserDao {
+
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public HashMap<String, String> getAll() {
+        return null;
+    }
+}
