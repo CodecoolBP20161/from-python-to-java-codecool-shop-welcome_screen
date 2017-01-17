99f6095a67605fbab88aad28f5a97910e96d2f1a
seradam <seradam89@gmail.com>
2017-01-17 14:32:48 +0100

    lottery controller + dependencies

--- src/main/java/com/codecool/shop/controller/LotteryController.java
+++ src/main/java/com/codecool/shop/controller/LotteryController.java
@@ -1,7 +1,28 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.Implementation.JdbcImpl.UserDaoJdbc;
+import org.json.JSONObject;
+import spark.Request;
+
+import java.io.IOException;
+import org.apache.http.client.utils.URIBuilder;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+
 /**
  * Created by seradam on 2017.01.17..
  */
 public class LotteryController {
+    private static final String SERVICE_URL = "http://localhost:60000";
+
+    public JSONObject getUsersFromDatabase(){
+        UserDaoJdbc datahandler = new UserDaoJdbc();
+        return new JSONObject(datahandler.getAllUserData());
+    }
+
+    private String execute(String url) throws IOException, URISyntaxException {
+        URI uri = new URIBuilder(SERVICE_URL).build();
+        return org.apache.http.client.fluent.Request.Get(uri).execute().returnContent().asString();
+    }
 }
