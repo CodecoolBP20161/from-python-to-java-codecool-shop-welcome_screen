da22932ab7cd796c6b3642f134f9966299d104f1
seradam <seradam89@gmail.com>
2017-01-17 10:57:50 +0100

    mickroservice skeleton

---
+++ src/main/java/com/codecool/lotteryservice/LotteryService.java
@@ -0,0 +1,7 @@
+package com.codecool.lotteryservice;
+
+/**
+ * Created by seradam on 2017.01.17..
+ */
+public class LotteryService {
+}
---
+++ src/main/java/com/codecool/lotteryservice/service/APIService.java
@@ -0,0 +1,7 @@
+package com.codecool.lotteryservice.service;
+
+/**
+ * Created by seradam on 2017.01.17..
+ */
+public class APIService {
+}
